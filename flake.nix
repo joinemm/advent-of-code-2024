32cf2dd6ddc7b133669ff680dc23a43eb80f33b3
Joonas Rautiola <joonas@rautiola.co>
2024-11-29 17:12:34 +0200

    Hello world!

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    let
+      pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [ zig ];
+      };
+    };
+}
