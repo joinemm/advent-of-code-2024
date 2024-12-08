f63d7172304125b7a41ee6af8f9c605d68f941bf
Joonas Rautiola <joonas@rautiola.co>
2024-12-08 15:36:22 +0200

    Fix graph formatting

--- graph.sh
+++ graph.sh
@@ -9,8 +9,12 @@ done
 # shellcheck disable=SC2046
 hyperfine $(echo "$days" | xargs -I{} echo "zig-out/bin/{}") -N --warmup 1 --export-csv bench.csv
 
+echo '```zig' >README.md
+
 tail -n+2 bench.csv |
     awk -F, 'BEGIN { OFS="," } { gsub("zig-out/bin/", "Day ", $1); $2 = sprintf("%.2f", $2 * 1000); print $0 }' |
-    uplot bar -d, --xscale log --width 100 --xlabel "Mean runtime in ms [log]" -o README.md
+    uplot bar -d, --xscale log --width 75 --xlabel "Mean runtime in ms [log]" -o >>README.md
+
+echo '```' >>README.md
 
 rm bench.csv
